0deb7e6b9d7a1e1db645997c19706e88515ba476
Otto <ottobrunok@gmail.com>
2021-11-22 03:16:05 +0200

    footer+header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,71 @@
 <template>
-  <p>Footer</p>
+  <footer>
+    <div class="footer-content">
+      <p class="head">Find us on</p>
+      <ul class="social">
+        <li><a href="#" class="facebook" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a>
+        </li>
+        <li><a href="#" class="twitter" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a>
+        </li>
+        <li><a href="#" class="instagram" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a>
+        </li>
+      </ul>
+      <p class="copy">&copy; 2021</p>
+    </div>
+  </footer>
 </template>
+<style>
+.fa-facebook::before {
+  content: url("https://lh3.googleusercontent.com/-VPr8buUo47w/VjMsRPIzr-I/AAAAAAAAAL4/AYBtvlNCQiw/s64-Ic42/clouds_social_media_icons_set_64x64_0000_facebook.png");
+}
+.fa-twitter::before {
+  content: url("https://lh3.googleusercontent.com/-yDH1FuHcQ5s/VjMsQ2lXvxI/AAAAAAAAAMg/i8JHsTh6aU8/s64-Ic42/clouds_social_media_icons_set_64x64_0002_twitter.png");
+}
+.fa-instagram::before {
+  content: url("https://lh3.googleusercontent.com/-VIveL13ocQc/VjMw7JU6dGI/AAAAAAAAAOo/AGxBey6rtC0/s64-Ic42/clouds_social_media_icons_set_64x64_0001_instagram.png")
+}
+footer{
+  overflow: hidden;
+  bottom: 50px;
+  left: 4%;
+  background: whitesmoke;
+  clear:both;
+  position: relative;
+  height: 210px;
+  width: 90%;
+  margin-top: 200px;
+  align-items: center;
+  color: whitesmoke;
+}
+.footer-content{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.head{
+  margin-top: 2.5%;
+  max-width: 500px;
+  line-height: 30px;
+  font-size: 30px;
+  color: dimgray;
+}
+.social{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.social li{
+  margin: 0 15px;
+}
+.copy{
+  margin-bottom: 50%;
+  max-width: 450px;
+  line-height: 10px;
+  font-size: 17px;
+  color: dimgray;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,98 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/contact">Contact</router-link>
-    <router-link to="/login">Login</router-link>
+  <div class="header">
+    <div class="dropdown">
+      <button class="dropdownButton" onclick="showInfo()">
+        <img
+            id="user-image"
+            src="images/me.png"
+            alt="user"
+            height="40"
+            display="block"
+        />
+      </button>
+      <div class="dropdown-content" id="dropdownInfo">
+        <b>John Doe</b>
+        <b>john.doe@example.com</b>
+      </div>
+    </div>
+      <div class="navbar">
+          <router-link class="nav-item nav-link" active-class="active" to="/">Home</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/contact">Contact</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/login">Login</router-link>
+    </div>
   </div>
-
 </template>
+<style>
+.header {
+  display: flex;
+  flex-direction: row;
+  background:whitesmoke;
+  width: 99%;
+}
+
+.header > div {
+  margin-left: 1%;
+  margin-right: 1%;
+}
+
+div + form {
+  margin-left: auto;
+  margin-right: auto;
+}
+.dropdown {
+  display: inline-block;
+}
+.dropdown .dropdownButton {
+  background: whitesmoke;
+  cursor: pointer;
+  position: relative;
+  height: 50px;
+
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  left:auto;
+  right:0%;
+  border-radius: 7px;
+  border: 5px;
+  background-color: whitesmoke;
+}
+.dropdown-content b {
+  color: lightslategray;
+  font-family: Arial;
+  float: none;
+  display: block;
+  text-align: left;
+  padding: 10px 10px 10px 10px;
+  border-radius: 7px;
+  border: 5px;
+}
+.dropdown-content b:hover{
+  background-color: lightgray;
+  color: dimgray;
+}
+
+.navbar{
+  overflow: hidden;
+}
+.nav-item {
+  float: left;
+  color: black;
+  font-family: arial;
+  text-align: center;
+  text-decoration: none;
+  padding: 15px 15px;
+  font-size: 18px;
+  padding-bottom: 11%;
+}
+.nav-item:hover{
+  background-color: lightblue;
+  color:darkslategrey;
+  text-decoration: none;
+}
+.nav-item{
+
+}
+
+</style>
