75f33cd3f6e7a2ba0e512e32c560a26f010d9254
Otto <ottobrunok@gmail.com>
2021-12-15 19:52:53 +0200

    navbar fix, new pages,images implement

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Posts' });
 });
 
 router.get('/login', function(req, res, next) {
@@ -13,8 +13,11 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
 
-router.get('/post', function(req, res, next) { //Todo add post id query 
-  res.render('post', { title: 'Post' });
+router.get('/singlepost', function(req, res, next) { //Todo add post id query
+  res.render('singlepost', { title: 'Post' });
+});
+router.get('/addnewpost', function(req, res, next) { //Todo add post id query
+  res.render('addnewpost', { title: 'New Post' });
 });
 
 module.exports = router;
